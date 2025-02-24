abe80d8a67281d99b79a2a48392e7a10df933b40
Fredev01 <fredev08@gmail.com>
2025-02-24 12:33:49 -0600

    First version of class TotalDissolvedSolids

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,19 @@
 #include <Arduino.h>
+#include "Sensors/TotalDissolvedSolids.h"
 
-// put function declarations here:
-int myFunction(int, int);
+TotalDissolvedSolids sensorTDS(A0);
+unsigned long timeout = 0;
 
 void setup()
 {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  sensorTDS.setup();
 }
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y)
-{
-  return x + y;
+  if (timeout < millis())
+  {
+    Serial.println(sensorTDS.getReading(6.0));
+    timeout = millis() + 1000;
+  }
 }
