b10a17fa017d04ef36df0e9cfc1e0b7c93331bec
Shado <mraul7875@gmail.com>
2025-04-09 11:31:48 -0600

    Comunicacion entre esp y arduino

---
+++ src/Sensors/SerialComm.cpp
@@ -0,0 +1,58 @@
+#include "SerialComm.h"
+
+SerialComm::SerialComm(HardwareSerial &serial) : serialPort(serial) {}
+
+void SerialComm::begin(long baudRate)
+{
+    serialPort.begin(baudRate);
+}
+
+void SerialComm::send(const String &message)
+{
+    serialPort.println(message);
+}
+
+int SerialComm::receive(KeyValue *data)
+{
+    if (!serialPort.available())
+        return 0;
+
+    String line = readLine();
+    line.trim();
+
+    int count = 0;
+    int start = 0;
+
+    while (start < line.length() && count < MAX_PAIRS)
+    {
+        int commaIndex = line.indexOf(',', start);
+        String pair = (commaIndex == -1) ? line.substring(start) : line.substring(start, commaIndex);
+        int equalIndex = pair.indexOf('=');
+
+        if (equalIndex != -1)
+        {
+            data[count].key = pair.substring(0, equalIndex);
+            data[count].value = pair.substring(equalIndex + 1);
+            count++;
+        }
+
+        if (commaIndex == -1)
+            break;
+        start = commaIndex + 1;
+    }
+
+    return count;
+}
+
+String SerialComm::readLine()
+{
+    String line = "";
+    while (serialPort.available())
+    {
+        char c = serialPort.read();
+        if (c == '\n')
+            break;
+        line += c;
+    }
+    return line;
+}
